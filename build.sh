a630af762c6d7315c47e97ee969ef08e8e77035e
jonathanmarklester <jonathanmarklester@gmail.com>
2019-06-28 18:04:56 +0100

    improved build script

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-date=$(date '+%Y%m%d')
 arch=$(uname -m)
 
 if [ $arch == "armv7l" ]; then
-  sed  's/__base_image__/armhf\/alpine/g' Dockerfile > Dockerfile.armhf
-  sudo docker image build -t j0nnylester/speedtest-cli:$date-armhf -f Dockerfile.armhf .
-  sudo docker image push j0nnylester/speedtest-cli:$date-armhf
-  rm Dockerfile.armhf
+  build_arch="armhf"
+  sed  's/__base_image__/armhf\/alpine/g' Dockerfile > Dockerfile.$build_arch
 fi
+
+if [ $arch == "x86_64" ]; then
+  build_arch="amd64"
+  sed  's/__base_image__/alpine/g' Dockerfile > Dockerfile.$build_arch
+fi
+
+date=$(date '+%Y%m%d')
+sudo docker image build -t j0nnylester/speedtest-cli:$date-$build_arch -t j0nnylester/speedtest-cli:latest-$build_arch -f Dockerfile.$build_arch .
+sudo docker image push j0nnylester/speedtest-cli:$date-$build_arch
+rm Dockerfile.$build_arch
